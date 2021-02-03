2226d5532b5850e36385763267152be2bcfb0ff4
jorgevelez <jorgevelez@localprojects.com>
2021-02-03 14:54:04 -0500

    try test

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+const app = require('express')();
+const http = require('http').Server(app);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
