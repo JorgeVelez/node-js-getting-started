97d0b49a2ef28478f183b6d1cf3f296d9c5e4e4d
jorgevelez <jorgevelez@localprojects.com>
2021-02-03 15:11:35 -0500

    stream test

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const app = require('express')();
-const http = require('http').Server(app);
+const http = require('http').Server(app),
+url = require('url'),
+fs   = require('fs'),
+filePath = 'audio.mp3',
+stat = fs.statSync(filePath);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', (request, response) => {
+    var queryData = url.parse(request.url, true).query;
+    const skip = typeof (queryData.skip) == 'undefined' ? 0 : queryData.skip;
+    console.log("stat "+stat.size);
+    const startByte = Math.floor(stat.size * skip);
+
+    response.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size - startByte
+    });
+
+    // We replaced all the event handlers with a simple call to util.pump()
+    fs.createReadStream(filePath, { start: startByte }).pipe(response);
 });
 
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
 });
+
